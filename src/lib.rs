1e565b134889b037f118e1294a84528553552119
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2023-10-05 10:47:32 +0200

    `CHANGELOG.md` update and version bump `v1.0.0`

--- src/lib.rs
+++ src/lib.rs
@@ -53,7 +53,7 @@
 //! notation.
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/ryu-js/0.2.2")]
+#![doc(html_root_url = "https://docs.rs/ryu-js/1.0.0")]
 #![allow(
     clippy::cast_lossless,
     clippy::cast_possible_truncation,
