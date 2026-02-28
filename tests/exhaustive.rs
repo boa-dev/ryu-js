dtolnay/ryu

Fix stacked borrows violation in test_exhaustive

tests/exhaustive.rs
@@ -37,7 +37,7 @@ fn test_exhaustive() {
                 if !f.is_finite() {
                     continue;
                 }
-                let n = unsafe { ryu::raw::format32(f, &mut bytes[0]) };
+                let n = unsafe { ryu::raw::format32(f, bytes.as_mut_ptr()) };
                 assert_eq!(Ok(Ok(f)), str::from_utf8(&bytes[..n]).map(str::parse));
                 assert_eq!(Ok(f), buffer.format_finite(f).parse());
             }

boa-dev/ryu-js

tests/exhaustive.rs
@@ -37,7 +37,7 @@ fn test_exhaustive() {
                 if !f.is_finite() {
                     continue;
                 }
-                let n = unsafe { ryu_js::raw::format32(f, &mut bytes[0]) };
+                let n = unsafe { ryu_js::raw::format32(f, bytes.as_mut_ptr()) };
                 assert_eq!(Ok(Ok(f)), str::from_utf8(&bytes[..n]).map(str::parse));
                 assert_eq!(Ok(f), buffer.format_finite(f).parse());
             }