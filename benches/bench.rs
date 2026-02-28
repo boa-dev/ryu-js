dtolnay/ryu

Switch from cargo bench to criterion

benches/bench.rs
@@ -1,58 +1,42 @@
-// cargo bench
-
-#![feature(test)]
-
-extern crate test;
-
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::fmt::Display;
 use std::hint;
 use std::io::Write;
 use std::{f32, f64};
-use test::Bencher;
 
-macro_rules! benches {
-    ($($name:ident($value:expr),)*) => {
-        mod bench_ryu {
-            use super::*;
-            $(
-                #[bench]
-                fn $name(b: &mut Bencher) {
-                    let mut buf = ryu::Buffer::new();
-
-                    b.iter(move || {
-                        let value = hint::black_box($value);
-                        let formatted = buf.format_finite(value);
-                        hint::black_box(formatted);
-                    });
-                }
-            )*
-        }
-
-        mod bench_std_fmt {
-            use super::*;
-            $(
-                #[bench]
-                fn $name(b: &mut Bencher) {
-                    let mut buf = Vec::with_capacity(20);
-
-                    b.iter(|| {
-                        buf.clear();
-                        let value = hint::black_box($value);
-                        write!(&mut buf, "{}", value).unwrap();
-                        hint::black_box(buf.as_slice());
-                    });
-                }
-            )*
-        }
-    };
+fn do_bench(c: &mut Criterion, group_name: &str, float: impl ryu::Float + Display) {
+    let mut group = c.benchmark_group(group_name);
+    group.bench_function("ryu", |b| {
+        let mut buf = ryu::Buffer::new();
+        b.iter(move || {
+            let float = hint::black_box(float);
+            let formatted = buf.format_finite(float);
+            hint::black_box(formatted);
+        });
+    });
+    group.bench_function("std::fmt", |b| {
+        let mut buf = Vec::with_capacity(20);
+        b.iter(|| {
+            buf.clear();
+            let float = hint::black_box(float);
+            write!(&mut buf, "{float}").unwrap();
+            hint::black_box(buf.as_slice());
+        });
+    });
+    group.finish();
 }
 
-benches! {
-    bench_0_f64(0f64),
-    bench_short_f64(0.1234f64),
-    bench_e_f64(f64::consts::E),
-    bench_max_f64(f64::MAX),
-    bench_0_f32(0f32),
-    bench_short_f32(0.1234f32),
-    bench_e_f32(f32::consts::E),
-    bench_max_f32(f32::MAX),
+fn bench(c: &mut Criterion) {
+    do_bench(c, "f64[0]", 0f64);
+    do_bench(c, "f64[short]", 0.1234f64);
+    do_bench(c, "f64[e]", f64::consts::E);
+    do_bench(c, "f64[max]", f64::MAX);
+
+    do_bench(c, "f32[0]", 0f32);
+    do_bench(c, "f32[short]", 0.1234f32);
+    do_bench(c, "f32[e]", f32::consts::E);
+    do_bench(c, "f32[max]", f32::MAX);
 }
+
+criterion_group!(benches, bench);
+criterion_main!(benches);

boa-dev/ryu-js

benches/bench.rs
@@ -1,54 +1,42 @@
-// cargo bench
-
 use criterion::{criterion_group, criterion_main, Criterion};
+use std::fmt::Display;
 use std::hint;
 use std::io::Write;
+use std::{f32, f64};
 
-macro_rules! benches {
-    ($($name:ident($value:expr),)*) => {
-        mod bench_ryu_js {
-            use super::*;
-            $(
-                pub fn $name(c: &mut Criterion) {
-                    let mut buf = ryu_js::Buffer::new();
-
-                    c.bench_function(concat!("ryu_js_", stringify!($name)), move |b| b.iter(move || {
-                        let value = hint::black_box($value);
-                        let formatted = buf.format_finite(value);
-                        hint::black_box(formatted);
-                    }));
-                }
-            )*
-        }
-        criterion_group!(bench_ryu_js, $( bench_ryu_js::$name, )*);
-
-        mod bench_std_fmt {
-            use super::*;
-            $(
-                pub fn $name(c: &mut Criterion) {
-                    let mut buf = Vec::with_capacity(20);
-
-                    c.bench_function(concat!("std_fmt_", stringify!($name)), move |b| b.iter(|| {
-                        buf.clear();
-                        let value = hint::black_box($value);
-                        write!(&mut buf, "{}", value).unwrap();
-                        hint::black_box(buf.as_slice());
-                    }));
-                }
-            )*
-        }
-        criterion_group!(bench_std_fmt, $( bench_std_fmt::$name, )*);
-        criterion_main!(bench_ryu_js, bench_std_fmt);
-    };
+fn do_bench(c: &mut Criterion, group_name: &str, float: impl ryu_js::Float + Display) {
+    let mut group = c.benchmark_group(group_name);
+    group.bench_function("ryu_js", |b| {
+        let mut buf = ryu_js::Buffer::new();
+        b.iter(move || {
+            let float = hint::black_box(float);
+            let formatted = buf.format_finite(float);
+            hint::black_box(formatted);
+        });
+    });
+    group.bench_function("std::fmt", |b| {
+        let mut buf = Vec::with_capacity(20);
+        b.iter(|| {
+            buf.clear();
+            let float = hint::black_box(float);
+            write!(&mut buf, "{float}").unwrap();
+            hint::black_box(buf.as_slice());
+        });
+    });
+    group.finish();
 }
 
-benches! {
-    bench_0_f64(0_f64),
-    bench_short_f64(0.1234_f64),
-    bench_e_f64(std::f64::consts::E),
-    bench_max_f64(f64::MAX),
-    bench_0_f32(0_f32),
-    bench_short_f32(0.1234_f32),
-    bench_e_f32(std::f32::consts::E),
-    bench_max_f32(f32::MAX),
+fn bench(c: &mut Criterion) {
+    do_bench(c, "f64[0]", 0f64);
+    do_bench(c, "f64[short]", 0.1234f64);
+    do_bench(c, "f64[e]", f64::consts::E);
+    do_bench(c, "f64[max]", f64::MAX);
+
+    do_bench(c, "f32[0]", 0f32);
+    do_bench(c, "f32[short]", 0.1234f32);
+    do_bench(c, "f32[e]", f32::consts::E);
+    do_bench(c, "f32[max]", f32::MAX);
 }
+
+criterion_group!(benches, bench);
+criterion_main!(benches);